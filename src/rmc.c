9beb5e21c2a96495dbf9290a3ccee2c54fead0a9
Christian Ehrig <christian.ehrig@eog-group.com>
2018-05-22 22:54:23 +0200

    indents

--- src/rmc.c
+++ src/rmc.c
@@ -7,36 +7,39 @@
 
 int main(int argc, char ** argv)
 {
-    int c, index;
-    char *port;
+	int c, index;
+	char *port;
 
-    opterr = 0;
-    while ((c = getopt (argc, argv, "p:")) != -1)
-    {
-        switch (c)
-        {
-            case 'p':
-                port = optarg;
-                        break;
-            case '?':
-                if (optopt == 'p')
-                    fprintf (stderr,
-                             "Option -%c requires an argument.\n", optopt);
-                else if (isprint (optopt))
-                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-                else
-                    fprintf (stderr,
-                             "Unknown option character `\\x%x'.\n", optopt);
-                        return 1;
-            default:
-                abort ();
-        }
-    }
+	opterr = 0;
+	while ((c = getopt (argc, argv, "p:")) != -1)
+	{
+		switch (c)
+		{
+			case 'p':
+				port = optarg;
+				break;
+			case '?':
+				if (optopt == 'p')
+					fprintf (stderr,
+						 "Option -%c requires an argument.\n", optopt);
+				else if (isprint (optopt))
+					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				else
+					fprintf (stderr,
+						 "Unknown option character `\\x%x'.\n", optopt);
+				return 1;
+			default:
+				abort ();
+		}
+	}
 
 
-    for (index = optind; index < argc; index++)
-        printf ("Non-option argument %s\n", argv[index]);
+	for (index = optind; index < argc; index++)
+	{
+		printf ("Non-option argument %s\n", argv[index]);
+	}
 
-    printf("-> %s \n", port);
-    return 0;
+
+	printf("-> %s \n", port);
+	return 0;
 }
