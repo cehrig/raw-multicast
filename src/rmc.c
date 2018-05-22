fc3d72843245e2f35311f202b01520f6ab112b8a
Christian Ehrig <christian.ehrig@eog-group.com>
2018-05-22 22:14:01 +0200

    autoconf

---
+++ src/rmc.c
@@ -0,0 +1,5 @@
+//
+// Created by cehrig on 5/22/18.
+//
+
+#include "rmc.h"
---
+++ src/rmc.h
@@ -0,0 +1,8 @@
+//
+// Created by cehrig on 5/22/18.
+//
+
+#ifndef RAW_MULTICAST_RMC_H
+#define RAW_MULTICAST_RMC_H
+
+#endif //RAW_MULTICAST_RMC_H
